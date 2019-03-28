2fdc3a04f759a94f28bf72f3903945572b318c01
Ronald Luna <ronaldluna1@gmail.com>
2019-03-28 00:43:28 -0600

    My Parents companies site IMAGES

--- index.html/index.js
+++ index.html/index.js
@@ -11,18 +11,19 @@ function currentSlide(n) {
 	showSlides(slideIndex = n);
 }
 
-
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
+function showSlides(n) {
 	var i;
-	var slides = document.getElementsByClassName("img");
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = slides.length}
 	for (i = 0; i < slides.length; i++) {
 		slides[i].style.display = "none";
 	}
-	slideIndex++;
-	if (slideIndex > slides.length) {slideIndex = 1}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
 	slides[slideIndex-1].style.display = "block";
-	setTimeout(showSlides, 2000); // Change image every 2 seconds
+	dots[slideIndex-1].className += " active";
 }
+
